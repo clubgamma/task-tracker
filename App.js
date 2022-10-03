224ea528cbea2b3eb39de12ed5461124e1d2ca1b
Farhan Ali <p200019@pwr.nu.edu.pk>
2022-10-03 17:12:59 +0500

    added and setup react navigation

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-
+import Header from "./components/Header";
+import Login from "./components/screens/Login";
+import Signup from "./components/screens/Signup";
+import Home from "./components/screens/Home";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>
-        Start your open source journey with this simple yet exciting project.
-        All the best!
-      </Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+    <View style={styles.body}>
+    <Header/>
+    <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Signup" component={Signup} />
+      </Stack.Navigator>
     </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    
+  },
+  body: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#fffaf0",
   },
 });

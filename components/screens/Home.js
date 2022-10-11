edcccf5874b8173236e626f7ec2e585f8d9a19d8
Sahil Totala <81063988+Flamingloon@users.noreply.github.com>
2022-10-11 22:06:02 +0530

    Update Home.js

--- components/screens/Home.js
+++ components/screens/Home.js
@@ -1,9 +1,50 @@
-import React from 'react'
-import {View} from 'react-native'
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 function Home() {
+  const [tasks, setTasks] = useState([
+    {
+      taskTitle: "Task#1",
+    },
+    {
+      taskTitle: "Task#2",
+    },
+  ]);
+  useEffect(() => {}, []);
   return (
-    <View></View>
-  )
+    <View>
+      <Text style={styles.welcome}>Hello, Your tasks of the day !</Text>
+      <View style={styles.taskContainer}>
+        {tasks.map((item, index) => {
+          return <Task key={index} taskTitle={item.taskTitle} />;
+        })}
+      </View>
+    </View>
+  );
 }
 
-export default Home
+const Task = ({ taskTitle }) => {
+  return (
+    <View style={styles.task}>
+      <Text>{taskTitle}</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  welcome: {
+    padding: 10,
+    fontSize: 20,
+    fontWeight: "400",
+  },
+  taskContainer: {
+    paddingHorizontal: 20,
+  },
+  task: {
+    marginVertical: 4,
+    padding: 10,
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 2,
+    elevation: 2,
+  },
+});
+export default Home;

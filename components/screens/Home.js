3b49ce3560cd48d6bde5001f6f9877ba86c6a284
Farhan Ali <p200019@pwr.nu.edu.pk>
2022-10-20 11:55:10 +0500

    added a link to the Tasks screen

--- components/screens/Home.js
+++ components/screens/Home.js
@@ -1,34 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-function Home() {
-  const [tasks, setTasks] = useState([
-    {
-      taskTitle: "Task#1",
-    },
-    {
-      taskTitle: "Task#2",
-    },
-  ]);
-  useEffect(() => {}, []);
+import { StyleSheet, Text, View,TouchableOpacity } from "react-native";
+import Task from "./Task";
+function Home({ navigation }) {
   return (
-    <View>
-      <Text style={styles.welcome}>Hello, Your tasks of the day !</Text>
-      <View style={styles.taskContainer}>
-        {tasks.map((item, index) => {
-          return <Task key={index} taskTitle={item.taskTitle} />;
-        })}
+    <View style={{flex:1,justifyContent:'flex-start',alignItems:'center'}}>
+      <TouchableOpacity
+              onPress={() => {
+                navigation.navigate("Task");
+              }}
+            >
+              <Text style={{ color: "green", marginTop: 6, fontSize:20,fontWeight:'bold' }}>
+                Click to Open Tasks!
+              </Text>
+            </TouchableOpacity>
+        
       </View>
-    </View>
+    
   );
 }
 
-const Task = ({ taskTitle }) => {
-  return (
-    <View style={styles.task}>
-      <Text>{taskTitle}</Text>
-    </View>
-  );
-};
+
 const styles = StyleSheet.create({
   welcome: {
     padding: 10,
@@ -41,7 +32,7 @@ const styles = StyleSheet.create({
   task: {
     marginVertical: 4,
     padding: 10,
-    borderColor: "black",
+    borderColor: "yellow",
     borderWidth: 1,
     borderRadius: 2,
     elevation: 2,
